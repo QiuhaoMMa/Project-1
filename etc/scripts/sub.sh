6420506b97c7a37bf12531117680bf429ac57e37
QiuhaoMMa <qima@siue.edu>
2024-01-26 22:53:15 -0500

    add sub.sh script

--- etc/scripts/sub.sh
+++ etc/scripts/sub.sh
@@ -1,14 +1,15 @@
+function sub() {
+    local topic_name
+    local file_name
+    local rate
 
+    read -p "Type the topic_name: " topic_name
+    read -p "Type the file_name.csv: " file_name
+    read -p "Enter the rate at which data is published: " rate
+    
+    touch "$file_name"
 
-subscribe_and_log() {
-  # Ask for topic_name
-  read -p "Type the topic_name: " topic_name
-
-  # Ask for file_name
-  read -p "Type the file_name.cvs: " file_name
-
-  # Ask for rate
-  read -p "Type the rate at which data is published: " rate
-
-  sub $topic_name $file_name --rate $rate
+    ros2 topic echo "$topic_name" --rate "$rate" --append >> "$file_name" &
 }
+
+sub
