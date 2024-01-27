210e8c58e408e7a5699a4434f5fe6c254d7b0c66
QiuhaoMMa <qima@siue.edu>
2024-01-26 22:54:53 -0500

    add pub.sh script

--- etc/scripts/pub.sh
+++ etc/scripts/pub.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-publish_command() {
+function pub() {
+
   local opt="$1"
   local topic_name="$2"
   local msg_type="$3"
   local msg="$4"
 
-  # Your logic for generating the publisher command goes here
-  echo "Publishing: pub $opt $topic_name $msg_type $msg"
+ros2 topic pub $opt $topic_name $msg_type "$msg" 
 }
+
+
+
+
+pub "--once" "/turtle1/cmd_vel" "geometry_msgs/msg/Twist" "{linear: {x: 0.0, y: 4.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -0.0}}"
+pub "--once" "/turtle1/cmd_vel" "geometry_msgs/msg/Twist" "{linear: {x: 0.0, y: 5.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -5.5}}"
+
+pub "--rate 100" /drone/pose geometry_msgs/msg/Pose "{position: {x: 2.0, y: 0.0, z: 0.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 1.8}}"
+ 
